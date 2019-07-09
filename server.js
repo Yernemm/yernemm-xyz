042b70aac8ed4b859772a7745c84e5f5c85ff2e3
Yernemm <yernemm@yernemm.xyz>
2019-07-09 18:03:21 +0100

    Initial

---
+++ server.js
@@ -0,0 +1,78 @@
+const http = require('http');
+const fs = require('fs');
+const url = require('url');
+
+const port = 3000;
+const siteDir = './site'
+const page404 = fs.readFileSync(siteDir + '/404.html');
+const indexPage = siteDir + '/index.html';
+
+const server = http.createServer((req,res)=>{
+    
+    console.log('[' + Date.now() + '] (' + req.connection.remoteAddress + ') ' + req.url);
+
+    var q = url.parse(req.url, true);
+    var filename = siteDir + q.pathname;
+
+    //Display index.html if no file name.
+    if(q.pathname == '/')
+    filename = indexPage
+
+    findPage(filename, (data,head)=>{
+        res.writeHead(head, {'Content-Type': 'text/html'});
+        res.write(data);
+        res.end();
+    })
+    
+})
+
+function findPage(filename, callback){
+    let data = 'Error.'
+    let head = 404
+    //First, try to find the full page name
+    fs.readFile(filename, (error, data)=>{
+        if(error){
+            //Not found. Let's try sticking a /index.html at the end.
+            let newFilename = filename; 
+            if (filename.endsWith('/'))
+                newFilename += 'index.html'
+            else
+                newFilename += '/index.html'
+
+                fs.readFile(newFilename, (error, data)=>{
+                    if(error){
+                        //Still not found. Let's try just adding a .html at the end
+
+                        fs.readFile(filename + '.html', (error, data)=>{
+                            if(error){
+                                //Not found after all this. Probably does not exist.
+                                //Display 404
+
+                                callback(page404, 404);
+
+                            }else{
+                                callback(data, 200);
+                            }
+                        })
+
+                    }else{
+                        callback(data, 200);
+                    }
+                })
+
+        } else{
+            callback(data, 200)
+        }
+       
+    })
+
+   
+}
+
+server.listen(port, (error)=>{
+    if(error){
+        console.error('An error occurred', error)
+    } else {
+        console.log('Server is listening on port ' + port)
+    }
+})
