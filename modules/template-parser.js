7e2f2e5e79597f8ade173eb77068fb1e03324567
Yernemm <yernemm@yernemm.xyz>
2019-07-11 00:59:09 +0100

    parser written

--- modules/template-parser.js
+++ modules/template-parser.js
@@ -1,15 +1,26 @@
 const fs = require('fs');
 const templateDir = './templates/';
 module.exports = parseHTML
+var allTemplates = [];
 
+fs.readdir(templateDir, (err, files) => {
+
+    files.forEach(f => {
+        let tempData = fs.readFileSync(templateDir + f, 'utf8');
+        let templateObj = {
+            file: f,
+            data: tempData
+        }
+        allTemplates.push(templateObj)
+    })
+
+  });
 
 function parseHTML(data, callback){
-
-    fs.readdir(templateDir, (err, files) => {
-        files.forEach(file => {
-          console.log(file);
-        });
-      });
-      let parsedData = ""
-   callback(parsedData)
+    allTemplates.forEach(temp => {
+        if(data.contains('<!--insert:' + temp.file + '-->')){
+            data = data.replace(new RegExp(temp.file), temp.data)
+        }
+    })
+   callback(data)
 }
