472f00099193d99fb58e698f2cd463b837a3a932
Yernemm <yernemm@yernemm.xyz>
2019-07-11 14:55:03 +0100

    hero icons - port owmn css gen

---
+++ toBePorted/projects/owmn-icons/index.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-87387417-1"></script>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-87387417-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>OWMN CSS Generator</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://www.w3schools.com/lib/w3-theme-indigo.css">
+    <script src="script.js"></script>
+    
+</head>
+
+<body class="w3-theme-l4">
+    <div class="w3-row">
+        <br>
+        <div class="w3-quarter w3-container w3-hide-medium w3-hide-small">
+        </div>
+        <div class="w3-half">
+            <div class="w3-card w3-theme-l3 w3-container">
+                <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+                <!-- Automatic -->
+                <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-2219001427204097" data-ad-slot="2452749368" data-ad-format="auto"
+                    data-full-width-responsive="true"></ins>
+                <script>
+                    (adsbygoogle = window.adsbygoogle || []).push({});
+                </script>
+            </div>
+            <br>
+            <div class="w3-card w3-theme-l3 w3-container">
+                <h1 class="w3-card w3-container w3-theme">OWMN CSS Generator</h1>
+                <h4 class="w3-text-theme w3-container">Generate the CSS and spritesheet for OWMN subreddits.</h4>
+                
+                <div class="w3-container w3-card w3-theme-l1">
+                    <h3>Colours:</h3>
+                    <form class="w3-container">
+                        Main colour hex code:
+                        <br>
+                        <input class="w3-input" type="text" name="colourMain" id="colourMain" placeholder="#dddddd">
+                        <br> Hover colour hex code:
+                        <br>
+                        <input class="w3-input" type="text" name="colourHover" id="colourHover" placeholder="#444444">
+                        <br>
+
+                    </form>
+                    <button onClick="generate();" class="w3-input w3-text-theme">Generate</button>
+                    <br>
+
+                </div>
+                <br>
+                <div class="w3-container w3-card w3-theme-l1">
+                    <h2>1) Sidebar:</h2>
+                    <p>Copy and paste the text below into your sidebar.</p>
+                    <textarea id="textSidebar" style="resize: none; width: 100%; height:200px;">
+                    </textarea>
+                    <br>
+                    <br>
+                </div>
+                <br>
+                <div class="w3-container w3-card w3-theme-l1">
+                    <h2>2) CSS:</h2>
+                    <p>Copy and paste the CSS below into your stylesheet.</p>
+                    <textarea id="textCSS" style="resize: none; width: 100%; height:200px;">
+                    </textarea>
+                    <br>
+                    <br>
+                </div>
+                <br>
+
+                <div class="w3-container w3-card w3-theme-l1">
+                    <h2>3) Sprite Sheets:</h2>
+                    <p>Upload these images with the given name.</p>
+                    <p id="spriteProgress"></p>
+                    <p>"herosprites"</p>
+                    <img id="spriteMain">
+                    <p>"herospriteshover"</p>
+                    <img id="spriteHover">
+                    <br>
+                    <br>
+                </div>
+                <br>
+                <p class="w3-text-theme w3-container">This page has been tested and made for Google Chrome. If you are having problems, I would recommend switching to Chrome.</p>
+                <p><a href="https://www.reddit.com/user/Yernemm">Made by /u/Yernemm</a></p>
+                <p><a href="https://yernemm.xyz/">Main Page</a></p>
+                <br>
+            </div>
+            <br>
+            <div class="w3-card w3-theme-l3 w3-container">
+                <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+                <!-- Automatic -->
+                <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-2219001427204097" data-ad-slot="2452749368" data-ad-format="auto"
+                    data-full-width-responsive="true"></ins>
+                <script>
+                    (adsbygoogle = window.adsbygoogle || []).push({});
+                </script>
+            </div>
+        </div>
+        <div class="w3-quarter w3-container w3-hide-medium w3-hide-small">
+        </div>
+        <br>
+    </div>
+
+</body>
+
+</html>
