22e3b934c6c82fe2c4488edd417f7a1e99161be0
Yernemm <yernemm@yernemm.xyz>
2019-07-12 02:01:50 +0100

    owet page

---
+++ toBePorted/projects/owet.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+	        <?php include($_SERVER['DOCUMENT_ROOT'].'/templates/head.php') ?>
+			
+			
+    </head>
+    <?php include($_SERVER['DOCUMENT_ROOT'].'/templates/defback.php') ?>
+	
+<?php include($_SERVER['DOCUMENT_ROOT'].'/templates/navp.php') ?>
+	
+        <div class="container">
+            <div class="Home">
+			<h1>OverWatch Extract Toolkit</h1>
+			                <hr>
+				</div>
+				            <div class="col-sm-8 blog-main">
+							<div style="margin: auto; display: block;">
+							<a type="button" style="display: block;" class="btn btn-lg btn-success"  href="/programs/OWExtractToolkit/OWET.zip" target="_blank">CLICK HERE TO DOWNLOAD</a>	
+			</div>
+			<br>
+			<p>This program provides a set of tools which can be used to extract files from Overwatch, including a GUI for DataTool. DataTool is a tool used to extract data from Overwatch (aka datamining). DataTool is a command prompt based tool so it can be difficult and time-consuming to use for people who are less experienced with command prompt. Because of this, the DataTool GUI in the OW Extract Toolkit provides a graphical user interface which allows you to use the main functionality of DataTool without typing in commands. It's as simple as clicking a button. I did not make DataTool itself, but I did create the GUI.</p>
+			<h3>Latest version: 0.6.1</h3>
+			<p>Simple setup instructions:<p>
+			<ol>
+	<li>Download the latest version of OW Extract Toolkit by clicking the download button above.</li>
+	<li>Extract OWExtractToolkit.exe anywhere.</li>
+	<li>Open OWExtractToolkit.exe</li>
+	<li>Open the settings menu and make sure the Overwatch game path is correct</li>
+	<li>Press the &quot;Download Latest DataTool&quot; in the settings</li>
+			<p><image src="https://i.imgur.com/flwuipT.png"></p>
+			<p><image src="https://i.imgur.com/R6dcwdR.png"></p>
+			
+			
+			
+			
+			
+			
+			
+			
+						<div>
+<?php include($_SERVER['DOCUMENT_ROOT'].'/templates/ad.php') ?>
+</div>
+<div>
+<?php include($_SERVER['DOCUMENT_ROOT'].'/templates/disqus.php') ?>
+</div>		
+			</div>
+					<!-- Sidebar Start-->
+        <?php include($_SERVER['DOCUMENT_ROOT'].'/templates/sidebar.php') ?>
+		</div>
+		<!-- Sidebar End-->
+        </div>
+        <!-- /.container -->
+        <!-- Bootstrap core JavaScript
+    ================================================== -->
+        <!-- Placed at the end of the document so the pages load faster -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+        <script src="/bootstrap/js/bootstrap.min.js"></script>
+        <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+        <script src="/bootstrap/js/ie10-viewport-bug-workaround.js"></script>
+		        <?php include($_SERVER['DOCUMENT_ROOT'].'/templates/foot.php') ?>
+    </body>
+</html>
